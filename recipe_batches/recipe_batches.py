c2728ca39b75cbd1ce44f6fecdf6cfe8ed1919dc
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-14 08:45:29 -0700

    completes recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,36 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    cur_max = 10 ** 100
+    for key, value in recipe.items():
+        if key not in ingredients or cur_max == 0:
+            return 0
+        cur_max = min(cur_max, ingredients[key] // value)
+    return cur_max
+
+# The above implementation isn't *guaranteed* to work. It will fail
+# if the maximum batches possible exceeds 10 ** 100. The implementation
+# below is guaranteed to work, but it allocates an additional list
+# in memory. Trade-offs, ya know?
+
+
+def recipe_batches_two(recipe, ingredients):
+    maxes = []
+    batches = 1
+    for key, value in recipe.items():
+        if batches == 0 or key not in ingredients:
+            return 0
+        batches = ingredients[key] // value
+        maxes.append(batches)
+    return min(maxes)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 99, 'butter': 200, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches_two(recipe, ingredients), ingredients=ingredients))
